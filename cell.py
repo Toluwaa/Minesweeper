e76c52011e8ef345bbbf815485d72daf25bf1731
KBTrotter <ktrotter@nevada.unr.edu>
2023-07-31 07:28:34 -0700

    Merging updated cell.py from main branch

--- cell.py
+++ cell.py
@@ -1,17 +1,50 @@
 from tkinter import Button
 
-
 class Cell:
-    def __init__(self, is_mine=False) -> None:
+    def __init__(self, x, y, is_mine=False) -> None:
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+        self.is_selected = False
+        self.is_flagged = False
+        self.is_uncovered = False
 
-    def create_btn_object(self, location):
+
+    def create_btn_object(self, parent):
         btn = Button(
-            location,
+            parent,
             width=5,
             height=2,
-            text=' '
+            text=f' '
         )
-        btn.bind()
+        btn.bind('<Button-1>', self.locate)
+        btn.bind('<Button-3>', self.locate)
+    
         self.cell_btn_object = btn
+    
+    #Updates the cell's text
+    def update_btn_text(self, text):
+        if self.cell_btn_object:
+            self.cell_btn_object.config(text=text)
+    
+    #Return the x and y of the cell
+    def locate(self, event):
+        print("Selected cells coords: ({x}, {y})".format(x=self.x, y=self.y))
+
+    #Set the cell as selected
+    def set_selected(self, selected=True):
+        self.is_selected = selected
+        if selected:
+            self.cell_btn_object.config(bg="lightblue")
+        else:
+            self.cell_btn_object.config(bg="lightgray")  # Restore default background color
+
+    #Set the cell as flagged  
+    def set_flagged(self, flagged=True):
+        self.is_flagged = flagged
+    
+    #logic for when the cell uncovers a mine
+    def show_mine(self):
+        self.cell_btn_object.config(bg="red")
+
